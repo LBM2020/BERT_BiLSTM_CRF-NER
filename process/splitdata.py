f5767ef56b10e5618b6877faeb77d728dd9c1f1a
LBM2020 <52284157+LBM2020@users.noreply.github.com>
2020-06-04 19:42:19 +0800

    Update splitdata.py

--- process/splitdata.py
+++ process/splitdata.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+from pathlib import Path
 from process.dataprocess import processer
 
 def split_data(args,file_name_or_path):
@@ -41,9 +42,11 @@ def split_data(args,file_name_or_path):
         for line in valid_samples:
             wf.write(line)
 
-def write_pre_result_to_file(args,preds,predict_label,pre_data):
-
-    with open(f'{args.output_dir}/predict_result/predict_result_focal_loss.txt', 'w') as wf:
-        for i, line in enumerate(pre_data):
-            pre_line = line.replace('\n','') + '\t' + str(preds[i]) + '\t' + str(predict_label[i])
-            wf.write(pre_line + '\n')
+def write_pre_result_to_file(args,predict_label):
+    path = Path(f'{args.output_dir}/predict_result')
+    if path not exists():
+        os.mkdir(path)
+    
+    with open(os.path.join(path,'predict_result.txt'), 'w') as wf:
+        for line in predict_label:
+            wf.write(line + '\n')
